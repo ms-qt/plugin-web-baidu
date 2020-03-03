6dd9b0bbf3915f8fa12200606e029180b52c2840
m-maohuawei <maohuawei@sina.com>
2020-03-03 20:27:28 +0800

    delete .idea

--- cmake-build-debug/CMakeFiles/plugin-web-baidu_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/plugin-web-baidu_autogen"
-  "plugin-web-baidu_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/plugin-web-baidu_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
